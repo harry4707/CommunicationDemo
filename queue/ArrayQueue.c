53c23f71e93aa795a184ba74e890d30bf9f87ed4
姚雨 <yaoyu.0891@bytedance.com>
2021-02-15 20:31:34 +0800

    debug
    Change-Id: Ie72a451aa69da479a2062cc5efb3de5a80bf7a83

--- queue/ArrayQueue.c
+++ queue/ArrayQueue.c
@@ -27,8 +27,8 @@
 int g_buffer[BUFFSIZE];
 unsigned short in = 0;// the point of production
 unsigned short out = 0;// the point of consumer
-unsigned short producer_id = 0;
-unsigned short consumer_id = 0;
+unsigned short produce_id = 0;
+unsigned short consume_id = 0;
 
 sem_t g_sem_full;//the count of buffer can be used
 sem_t g_sem_empty; // the count of production can be consumer
@@ -56,11 +56,11 @@ void *consume (void *arg) {
             printf("\n");
         }
         //produce()操作(生产产品)
-        consumer_id = g_buffer[out];
-        printf("consumer-%d begin consume product %d\n", consumer_id, num);
+        consume_id = g_buffer[out];
+        printf("consumer-%d begin consume product %d\n", num, consume_id);
         g_buffer[out] = -1;
         out = (out + 1) % BUFFSIZE; // to move the point to next position of production
-        printf("consumer-%d end consumer product %d \n", consumer_id, num);
+        printf("consumer-%d end consumer product %d \n", num, consume_id++);
         pthread_mutex_unlock(&g_mutex);
         sem_post(&g_sem_full);// use the semaphore to communicate reciprocally
         sleep(1);
@@ -72,7 +72,7 @@ void *produce(void *arg) {
     int num = (int) arg;
     int i;
     while (1) {
-        printf("%d wait buffer not full\n", num);
+        printf("producer-%d wait buffer not full\n", num);
         sem_wait(&g_sem_full);
         pthread_mutex_lock(&g_mutex);
         for (i = 0; i < BUFFSIZE; i++)
@@ -88,10 +88,10 @@ void *produce(void *arg) {
 
             printf("\n");
         }
-        printf("producer-%d begin produce product %d\n", producer_id, num);
-        g_buffer[in] = producer_id;
+        printf("producer-%d begin produce product %d\n", num, produce_id);
+        g_buffer[in] = produce_id;
         in = (in + 1) % BUFFSIZE;
-        printf("producer-%d end produce product %d\n", producer_id++, num);
+        printf("producer-%d end produce product %d\n", num, produce_id++);
         pthread_mutex_unlock(&g_mutex);
         sem_post(&g_sem_empty);
         sleep(5);
